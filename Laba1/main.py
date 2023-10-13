13d0c5cceb181d7dcf935c58c3478f53d4f1400e
Slain <d9198326303@gmail.com>
2023-10-13 21:59:58 +0300

    реализованна работа с файлами xml, пользовательское исключение проверки корректности возраста, getter age

--- Laba1/main.py
+++ Laba1/main.py
@@ -1,5 +1,16 @@
 import json
 from abc import ABC, abstractmethod
+import xml.etree.ElementTree as ET
+
+
+class AgeError(Exception):
+    def __init__(self, age, message='Должен быть от 0 до 150 лет'):
+        self.age = age
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f'Возраст {self.age}. {self.message}'
 
 
 class User(ABC):
@@ -9,7 +20,10 @@ class User(ABC):
         self._last_name = last_name
         self._email = email
         self._phone = phone
-        self._age = age
+        if 0 <= age <= 150:
+            self._age = age
+        else:
+            raise AgeError(age)
 
     @abstractmethod
     def write_info_to_file_json(self, file):
@@ -61,11 +75,36 @@ class Student(User):
         except FileNotFoundError:
             print('Ошибка при вызове функции get_json_from_file. ТАКОГО ФАЙЛА НЕ НАЙДЕНО! ПРОВЕРЬТЕ ПУТЬ!!!')
 
+    @property
+    def age(self):
+        return self._age
+
     def get_xml_from_file(self, file):
-        pass  # todo: realize this point
+        tree = ET.parse(file)
+        root = tree.getroot()
+        for elem in root:
+            for subelem in elem:
+                print(f"{subelem.tag}:{subelem.text}")
 
     def write_info_to_file_xml(self, file):
-        pass  # todo: realize this point
+        info = {
+            'id': self._id,
+            'first_name': self._first_name,
+            'last_name': self._last_name,
+            'age': self._age,
+            'email': self._email,
+            'phone': self._phone,
+            'education': self.__education,
+            'graduate_work': self.__graduate_work,
+            'direction_of_training': self.__direction_of_training
+        }
+        root = ET.Element('root')
+
+        teacher = ET.SubElement(root, 'teacher')
+        for key in info:
+            ET.SubElement(teacher, key).text = str(info[key])
+        tree = ET.ElementTree(root)
+        tree.write(file)
 
 
 class Teacher(User):
@@ -101,13 +140,41 @@ class Teacher(User):
         except FileNotFoundError:
             print('Ошибка при вызове функции get_json_from_file. ТАКОГО ФАЙЛА НЕ НАЙДЕНО! ПРОВЕРЬТЕ ПУТЬ!!!')
 
+    @property
+    def age(self):
+        return self._age
+
     def get_xml_from_file(self, file):
-        pass  # todo: realize this point
+        tree = ET.parse(file)
+        root = tree.getroot()
+        for elem in root:
+            for subelem in elem:
+                print(f"{subelem.tag}:{subelem.text}")
 
     def write_info_to_file_xml(self, file):
-        pass  # todo: realize this point
+        info = {
+            'id': self._id,
+            'first_name': self._first_name,
+            'last_name': self._last_name,
+            'age': self._age,
+            'email': self._email,
+            'phone': self._phone,
+            'department': self.__department,
+            'work_experience': self.__work_experience,
+            'subjects': self.__subjects
+        }
+        root = ET.Element('root')
+        teacher = ET.SubElement(root, 'teacher')
+        for key in info:
+            ET.SubElement(teacher, key).text = str(info[key])
+        subjects = ET.SubElement(teacher, 'subjects')
+        for i in range(len(info['subjects'])):
+            ET.SubElement(subjects, f'subject{i+1}').text = str(info['subjects'][i])
+        tree = ET.ElementTree(root)
+        tree.write(file)
 
 
-Elkin = Teacher(0, 'Alexandr', 'Elkin', 100, 'El@gmail.com', '+79084445569', 'Math', 99, ['math', 'linear algebra'])
-Elkin.write_info_to_file_json('files/info.json')
-info = Elkin.get_json_from_file('files/inf1o.json')
+Elkin = Teacher(0, 'Alexandr', 'Elkin', 30, 'El@gmail.com', '+79084445569', 'Math', 99, ['math', 'linear algebra'])
+Test = Student(0, 'Alexandr', 'Elkin', 30, 'El@gmail.com', '+79084445569', 1,1,1)
+Test.get_xml_from_file('files/test2.xml')
+
